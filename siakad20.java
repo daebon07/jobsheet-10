f8111ee59a4b03cee6e23d8e5ce0fc6c45b3a449
daebon07 <muhammadshabran25@gmail.com>
2024-11-07 11:15:32 +0700

    percobaan 4 setelah di modifikasi dan ditambah

--- siakad20.java
+++ siakad20.java
@@ -8,13 +8,29 @@ public class siakad20 {
 
                 for (int i=0; i<nilai.length; i++){
                     System.out.println("input nilai mahasiswa ke - "+(i+1));
+                    double totalpersiswa=0;
+
                     for (int j=0; j<nilai[i].length; j++){
                         System.out.println("nilai matakuliah "+(j+1)+" : ");
                         nilai [i][j]=sc.nextInt();
-                        
+                        totalpersiswa += nilai [i][j];
                     }
-
+                    System.out.println("nilai rata rata :" + totalpersiswa/3);
                 }
+
+             System.out.println("\n=======================================");
+            System.out.println("rata rata nilai setiap mata kuliah:");
+
+                for (int j=0; j<3; j++){
+                        double totalpermatkul=0;
+
+                        for (int i=0; i<4; i++){
+                            totalpermatkul += nilai [i][j];
+                        }
+
+                        System.out.println("matat kuliah" + (j+1) + " : " + totalpermatkul/4);
+
+                    }
 
 
             }
