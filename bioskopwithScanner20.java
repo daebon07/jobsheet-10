009138de93beb9cff234dfda063814d26ab547b2
daebon07 <muhammadshabran25@gmail.com>
2024-11-07 09:59:14 +0700

    modifikasi percobaan 2  pertanyaan nomor 5

--- bioskopwithScanner20.java
+++ bioskopwithScanner20.java
@@ -7,23 +7,28 @@ public class bioskopwithScanner20 {
         int baris, kolom;
         String nama, next;
 
+        
         String[][] penonton = new String[4][2];
 
         while (true) {
+
             System.out.println("\n--- Menu ---");
             System.out.println("1. Input data penonton");
             System.out.println("2. Tampilkan daftar penonton");
             System.out.print("Pilih menu (1/2): ");
             int pilihan = sc.nextInt();
             sc.nextLine(); 
+
             if (pilihan == 1) {
+            
                 System.out.print("Masukkan nama: ");
                 nama = sc.nextLine();
 
                 boolean kursiTersedia = false;
 
+               
                 while (!kursiTersedia) {
-                  
+
                     while (true) {
                         System.out.print("Masukkan baris (1-4): ");
                         baris = sc.nextInt();
@@ -34,37 +39,39 @@ public class bioskopwithScanner20 {
                         }
                     }
 
-                   
                     while (true) {
                         System.out.print("Masukkan kolom (1-2): ");
                         kolom = sc.nextInt();
                         if (kolom >= 1 && kolom <= 2) {
-                            break;
+                            break; 
                         } else {
                             System.out.println("Kolom tidak valid! Harap masukkan nilai antara 1 dan 2.");
                         }
                     }
                     sc.nextLine(); 
 
-                   
+
                     if (penonton[baris - 1][kolom - 1] != null) {
                         System.out.println("Peringatan: Kursi Baris " + baris + ", Kolom " + kolom + " sudah terisi!");
                         System.out.println("Silakan pilih kursi yang lain.");
                     } else {
-                      
+
                         penonton[baris - 1][kolom - 1] = nama;
                         System.out.println("Penonton berhasil ditambahkan!");
-                        kursiTersedia = true; 
+                        kursiTersedia = true;
                     }
                 }
 
             } else if (pilihan == 2) {
-  
+
                 System.out.println("\nDaftar Penonton:");
                 boolean adaPenonton = false;
                 for (int i = 0; i < penonton.length; i++) {
                     for (int j = 0; j < penonton[i].length; j++) {
-                        if (penonton[i][j] != null) {
+
+                        if (penonton[i][j] == null) {
+                            System.out.println("Baris " + (i + 1) + ", Kolom " + (j + 1) + ": ***");
+                        } else {
                             System.out.println("Baris " + (i + 1) + ", Kolom " + (j + 1) + ": " + penonton[i][j]);
                             adaPenonton = true;
                         }
@@ -77,7 +84,6 @@ public class bioskopwithScanner20 {
             } else {
                 System.out.println("Pilihan tidak valid. Silakan pilih menu 1 atau 2.");
             }
-
 
             System.out.print("\nIngin melanjutkan? (y/n): ");
             next = sc.nextLine();
