8f5cc3f889ce4a68eb453a45cd78aebb6da77e7f
daebon07 <muhammadshabran25@gmail.com>
2024-11-07 08:31:40 +0700

    percobaan 1

---
+++ bioskop20.java
@@ -0,0 +1,20 @@
+public class bioskop20 {
+
+    public static void main(String[] args) {
+        
+        String [][] penonton = new String [4][2];
+
+        penonton [0][0] = "amin";
+        penonton [0][1] = "bena";
+        penonton [1][0] = "candra";
+        penonton [1][1] = "dela";
+        penonton [2][0] = "eka";
+        penonton [2][1] = "farhan";
+        penonton [3][0] = "gisel";
+
+        System.out.printf( "%s \t %s \n", penonton[0][0], penonton [0][1]);
+        System.out.printf( "%s \t %s \n", penonton[1][0], penonton [1][1]);
+        System.out.printf( "%s \t %s \n", penonton[2][0], penonton [2][1]);
+        System.out.printf( "%s \t %s \n", penonton[3][0], penonton [3][1]);
+    }
+}
